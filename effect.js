f8cc414fe0e8854a853df9127c164bbe6490dfcc
cNassim <promail.nassim@gmail.com>
2024-05-11 21:59:34 +0100

    cmt

--- effect.js
+++ effect.js
@@ -15,34 +15,39 @@ $('document').ready(function(){
 			$('#b66').animate({top:240, left: vw+150},500);
 			$('#b77').animate({top:240, left: vw+250},500);
 		});
-
-	$('#turn_on').click(function(){
-		$('#bulb_yellow').addClass('bulb-glow-yellow');
-		$('#bulb_red').addClass('bulb-glow-red');
-		$('#bulb_blue').addClass('bulb-glow-blue');
-		$('#bulb_green').addClass('bulb-glow-green');
-		$('#bulb_pink').addClass('bulb-glow-pink');
-		$('#bulb_orange').addClass('bulb-glow-orange');
-		$('body').addClass('peach');
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#play').fadeIn('slow');
+		$('#initial').click(function(){
+			$(this).fadeOut('slow').delay(50).promise().done(function(){
+				$('#turn_on').fadeIn('slow');
+			});
 		});
-	});
-	$('#play').click(function(){
-		var audio = $('.song')[0];
-        audio.play();
-        $('#bulb_yellow').addClass('bulb-glow-yellow-after');
-		$('#bulb_red').addClass('bulb-glow-red-after');
-		$('#bulb_blue').addClass('bulb-glow-blue-after');
-		$('#bulb_green').addClass('bulb-glow-green-after');
-		$('#bulb_pink').addClass('bulb-glow-pink-after');
-		$('#bulb_orange').addClass('bulb-glow-orange-after');
-		$('body').css('backgroud-color','#FFF');
-		$('body').addClass('peach-after');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
-			$('#bannar_coming').fadeIn('slow');
+		$('#play').click(function(){
+			var audio = $('.song')[0];
+			audio.play();
+			$('#bulb_yellow').addClass('bulb-glow-yellow-after');
+			$('#bulb_red').addClass('bulb-glow-red-after');
+			$('#bulb_blue').addClass('bulb-glow-blue-after');
+			$('#bulb_green').addClass('bulb-glow-green-after');
+			$('#bulb_pink').addClass('bulb-glow-pink-after');
+			$('#bulb_orange').addClass('bulb-glow-orange-after');
+			$('body').css('backgroud-color','#FFF');
+			$('body').addClass('peach-after');
+			$(this).fadeOut('slow').delay(6000).promise().done(function(){
+				$('#bannar_coming').fadeIn('slow');
+			});
 		});
-	});
+		$('#turn_on').click(function(){
+			$('#bulb_yellow').addClass('bulb-glow-yellow');
+			$('#bulb_red').addClass('bulb-glow-red');
+			$('#bulb_blue').addClass('bulb-glow-blue');
+			$('#bulb_green').addClass('bulb-glow-green');
+			$('#bulb_pink').addClass('bulb-glow-pink');
+			$('#bulb_orange').addClass('bulb-glow-orange');
+			$('body').addClass('peach');
+			$("#welcome_dance").remove();
+			$(this).fadeOut('slow').delay(5000).promise().done(function(){
+				$('#play').fadeIn('slow');
+			});
+		});
 
 	$('#bannar_coming').click(function(){
 		$('.bannar').addClass('bannar-come');
